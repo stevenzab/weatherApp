4f32f8bc33013b812af74e52c77396e4f856fbdd
Steven ZABANDITH <steven.zabandith@epitech.eu>
2024-06-02 16:39:18 +0200

    [ADD] API_key

--- weatherapp/components/footer.tsx
+++ weatherapp/components/footer.tsx
@@ -5,24 +5,19 @@ export default function Footer() {
 		<footer className="bg-black text-white py-6">
 			<div className="max-w-7xl mx-auto px-4">
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-					{/* Navigation Links */}
 					<div>
 						<h4 className="text-lg font-semibold mb-4">Navigation</h4>
 						<ul>
 							<li><a href="#" className="hover:text-gray-400">Home</a></li>
 							<li><a href="#" className="hover:text-gray-400">About Us</a></li>
-							{/* Add more navigation links here */}
 						</ul>
 					</div>
-
-					{/* Contact Information */}
 					<div>
-						<h4 className="text-lg font-semibold mb-4">Contact Us</h4>
+						<h4 className="text-lg font-semibold mb-4">Contact Me</h4>
 						<p>Email: example@example.com</p>
 						<p>Phone: 123-456-7890</p>
 					</div>
 
-					{/* Social Media Links */}
 					<div>
 						<h4 className="text-lg font-semibold mb-4">Follow Me</h4>
 						<ul>
--- weatherapp/pages/index.tsx
+++ weatherapp/pages/index.tsx
@@ -1,12 +1,52 @@
-import Image from "next/image";
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchWeather() {
+
+      const apiKey = process.env.API_KEY;
+      const baseUrl = "http://api.weatherapi.com/v1";
+      const days = 5;
+      const endpoint = `${baseUrl}/forecast.json?key=${apiKey}&days=${days}&q=London`;
+      console.log("API Endpoint:", endpoint);
+
+      try {
+        const response = await fetch(endpoint);
+
+        if (response.ok) {
+          console.log('Response:', response);
+        } else {
+          console.error('Error:', response.statusText);
+          console.log(response)
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    fetchWeather();
+  }, []);
+
   return (
     <>
       <p className="flex justify-center text-center">
         Weather App
-
       </p>
+      {error && <p className="flex justify-center text-center text-red-500">{error}</p>}
+      {weatherData ? (
+        <div className="weather-container">
+          <p>Location: {weatherData}</p>
+          {/* <p>Temperature: {weatherData.current.temp_c}°C</p>
+          <p>Condition: {weatherData.current.condition.text}</p> */}
+          {/* Add more weather data as needed */}
+        </div>
+      ) : (
+        <p className="flex justify-center text-center">Loading...</p>
+      )}
     </>
   );
 }
